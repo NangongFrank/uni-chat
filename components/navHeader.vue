efb22548a2ab7e79c5f20e7c431dceb5ebb6b3a3
zhao mac <3345@qq.com>
2019-06-13 18:01:12 +0800

    my firends

--- components/navHeader.vue
+++ components/navHeader.vue
@@ -1,6 +1,16 @@
 <template>
 	<view class="content">
-
+		<view class="content-left">
+			
+		</view>
+		<view class="content-center">
+			
+		</view>
+		<view class="content-right">
+			<view class="search-wrapper">
+				<input confirm-type="search" placeholder="输入名称或ID">
+			</view>
+		</view>
 	</view>
 </template>
 <script>
@@ -15,5 +25,15 @@
 <style lang="less" scoped>
 	@import "../static/config.less";
 	@import "../static/theme/components/tabNav.less";
-	
+	.content {
+		display: flex;
+		padding-top: 20upx;
+		&-left,
+		&-right {
+			flex: 1;
+		}
+		&-center {
+			flex: 2;
+		}
+	}
 </style>
